c8d2f3fc45c07e174d78ab5c2881e28de118e2a7
gutierrezmarco19 <gutierrezmarco19@gmail.com>
2017-01-30 20:03:22 -0430

    Todo lo relacionado a maestro de sexo, hace falta bloquear el estatus al mostrarse, le agregue al navbar un chip que debe ser la imagen del usuario logeado

--- app/controllers/sexos_controller.rb
+++ app/controllers/sexos_controller.rb
@@ -69,6 +69,6 @@ class SexosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sexo_params
-      params.require(:sexo).permit(:descripcion)
+      params.require(:sexo).permit(:descripcion,:estatus)
     end
 end
