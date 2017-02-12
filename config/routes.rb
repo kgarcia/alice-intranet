b1840bc3a8bdec5c0f9b4705b963b91f94b943ae
mizukagetobi <kevin93ps@gmail.com>
2017-02-12 16:44:28 -0430

    prueba

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myapp::Application.routes.draw do
   get "home/index"
   get "home/minor"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
