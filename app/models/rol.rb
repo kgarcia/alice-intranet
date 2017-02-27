ac30ac8ef5396ab5d3973e53bd2a32664f045507
Jonnathan Gte <jonnathangte68@hotmail.com>
2017-02-27 17:00:09 -0400

    Update rol.rb

--- app/models/rol.rb
+++ app/models/rol.rb
@@ -1,23 +1,22 @@
 class Rol < ApplicationRecord
-	has_many :users
-	has_many :option_roles
-	has_many :option_menu, through: :option_roles
-	after_create :save_option_menus
+  has_many :users
+  has_many :option_roles
+  has_many :option_menu, through: :option_roles
+  after_create :save_option_menus
 attr_reader :option_menus
 
   def option_menus=(value)
-  	@option_menus = value
+    @option_menus = value
   end
 
   private
   def save_option_menus
-  	@option_menus.each do |option_menu_id|
-  		OptionRol.create(option_menu_id: option_menu_id, rol_id: self.id )
-  	end
-  		
+  if !@option_menus.nil?  
+    @option_menus.each do |option_menu_id|
+      OptionRol.create(option_menu_id: option_menu_id, rol_id: self.id )
+    end
   end
-
-  def self.titulo
-    return "Roles"
+      
   end
 end
+
