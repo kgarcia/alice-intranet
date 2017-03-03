3d77f9fdfcba31525d6ef9546a1812d4e9cffddd
Jonnathan Gte <jonnathangte68@hotmail.com>
2017-03-03 13:58:16 -0400

    Update 20170219021959_create_especialidades.rb

--- db/migrate/20170219021959_create_especialidades.rb
+++ db/migrate/20170219021959_create_especialidades.rb
@@ -3,6 +3,8 @@ class CreateEspecialidades < ActiveRecord::Migration[5.0]
     create_table :especialidades do |t|
       t.string :descripcion
       t.integer :estatus
+      t.references :tipo_especialidad, foreign_key: true
+      t.references :especialista, foreign_key: true
 
       t.timestamps
     end
