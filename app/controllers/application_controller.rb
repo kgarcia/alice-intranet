b1a7f6095874a2a1faeff10137937efdee4b2125
mizukagetobi <kevin93ps@gmail.com>
2017-03-14 00:25:12 -0430

    webservice opinion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery unless: -> { request.format.json? }
 
   #skip_before_action :verify_authenticity_token
-
+ #protect_from_forgery with: :exception
   #protect_from_forgery with: :null_session
   protect_from_forgery with: :null_session, if: ->{request.format.json?}
 
@@ -13,7 +13,21 @@ class ApplicationController < ActionController::Base
 
  before_action :configure_permitted_parameters, if: :devise_controller?
 
+ #before_action :authenticate
+ 
   protected
+  
+  def authenticate
+    token_str = params[:token]
+    token = Token.find_by(token: token_str)
+    
+    if token.nil? or not token.is_valid? or not @my_app.is_your_token?(token)
+      error!("Tu token es inválido", :unauthorized)
+    else
+      @current_user = token.user
+    end
+
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:rol_id])
