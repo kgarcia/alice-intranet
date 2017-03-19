9fbc925ec15a50ca50215153557fed20b86cbe09
Roberth <roberthebt@gmail.com>
2017-03-19 15:44:48 -0400

    Grafico de calificaciones por criterios

--- app/controllers/grafico_controller.rb
+++ app/controllers/grafico_controller.rb
@@ -7,7 +7,7 @@ class GraficoController < ApplicationController
   def citas_por_especialidad
   	@especialidades = Especialidad.contarCitas
   	@estadisticas =  @especialidades.descriptive_statistics
-    
+
     @tipoServicios = TipoServicio.contarCitas
     @estadisticasTipoServicios =  @tipoServicios.descriptive_statistics
 
@@ -45,8 +45,77 @@ class GraficoController < ApplicationController
   end
 
   def evaluaciones_por_paciente
-    @titulo = "Evaluaciones por paciente"
     @especialidades = Especialidad.all
     render "grafico/pie-basic"
   end
+
+  def calificaciones_por_criterio
+    @especialidades = Especialidad.all
+    #render "grafico/calificaciones_por_criterio"
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def citas_evaluadas_params
+    params.permit(:descripcion, :especialidad_id, :tipo_turno_id, :criterio_id, :fecha_inicio, :fecha_fin)
+  end
+
+  def generar_citas_evaluadas
+    #puts :especialidad_id.inspect
+    @calificacionesOK = []
+    @calificaciones = Calificacion.all
+    @calificacionesEspecialidades = []
+    @calificacionesCriterios = []
+    @criterio = Criterio.find(params[:criterio_id])
+    @data = Hash.new
+    @data["muymal"] = 0
+    @data["mal"] = 0
+    @data["regular"] = 0
+    @data["bueno"] = 0
+    @data["muybueno"] = 0
+    @calificaciones.each do |calificacion|
+      #validando data segun parametros seleccionados
+      @evaluacion = Evaluacion.find(calificacion.evaluacion_id)
+      @cita = Cita.find(@evaluacion.cita_id)
+      @turno = Turno.find(@cita.turno_id)
+      if @turno.tipo_turno_id.to_s.eql?(params[:tipo_turno_id])
+        @horario = Horario.find(@turno.horario_id)
+        @servicio = Servicio.find(@horario.servicio_id)
+        @tipo_servicio = TipoServicio.find(@servicio.tipo_servicio_id)
+        @calificacionesEspecialidades.push(calificacion)
+        if @tipo_servicio.especialidad_id.to_s.eql?(params[:especialidad_id])
+          @calificacionesCriterios.push(calificacion)
+          if calificacion.criterio_id.to_s.eql?(params[:criterio_id])
+            #preparando data de la grafica...
+            if calificacion.descripcion.eql? "1"
+              @data["muymal"] = @data["muymal"] + 1
+            end
+            if calificacion.descripcion.eql? "2"
+              @data["mal"] = @data["mal"] + 1
+            end
+            if calificacion.descripcion.eql? "3"
+              @data["regular"] = @data["regular"] + 1
+            end
+            if calificacion.descripcion.eql? "4"
+              @data["bueno"] = @data["bueno"] + 1
+            end
+            if calificacion.descripcion.eql? "5"
+              @data["muybueno"] = @data["muybueno"] + 1
+            end
+            @calificacionesOK.push(calificacion)
+          end
+          #FALTA EVALUAR QUE LAS CITAS TENGAN LA FECHA EN EL RANGO SELECCIONADO
+        end
+      end
+    end
+    puts "total grafica: " + @data.inspect
+    puts "Calificaciones totales: " + @calificaciones.length.to_s
+    puts "Calificaciones especificas: " + @calificacionesOK.length.to_s
+    puts "Calificaciones por especialidades: " + @calificacionesEspecialidades.length.to_s
+    puts "Calificaciones por criterios: " + @calificacionesCriterios.length.to_s
+    @totalCalificaciones = @calificaciones.length
+    @totalSeleccionado = ((@calificacionesOK.length.to_f / @totalCalificaciones) * 100).round(2)
+    @especialidades = Especialidad.all
+    render "grafico/calificaciones_por_criterio"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,9 @@ Myapp::Application.routes.draw do
   post 'perfil', to: 'usuarios#actualizarPerfil'
 
   get 'disponibilidad', to: 'horarios#disponibilidad'
-  
+
   match '/search', to: "personas#search", via: "post"
+  match '/generar_citas_evaluadas', to: "grafico#generar_citas_evaluadas", via: "post"
 
   get 'grafico/generar'
 
@@ -29,6 +30,7 @@ Myapp::Application.routes.draw do
   get 'grafico/citas_por_turno'
 
   get 'grafico/evaluaciones_por_paciente'
+  get 'grafico/calificaciones_por_criterio'
 
   get 'agenda_servicio/', to: 'agenda_servicio#lista'
 
@@ -56,7 +58,7 @@ Myapp::Application.routes.draw do
   resources :tipo_opiniones
   resources :tipo_preguntas
   resources :tipo_noticias
-  get 'usuarios', to: 'usuarios#index' 
+  get 'usuarios', to: 'usuarios#index'
 
 
   get 'agenda_servicio/:id/detalle', to: 'agenda_servicio#detalle', as: :agenda_servicio_detalle
@@ -88,17 +90,17 @@ Myapp::Application.routes.draw do
   resources :personas
   resources :roles
   devise_for :usuarios , :controllers => {registrations: "usuarios/registrations", sessions: "usuarios/sessions", confirmations: 'usuarios/confirmations'}
-  
+
   devise_scope :usuario do
     get 'login', to: 'usuarios/sessions#new'
-    
+
     authenticated :usuario do
       root to: 'home#index', as: :authenticated_root
     end
-    
+
   end
 
-  
+
 
   root to: 'usuarios/sessions#new'
 
@@ -149,7 +151,7 @@ Myapp::Application.routes.draw do
 
   get "home/index"
   get "home/minor"
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
